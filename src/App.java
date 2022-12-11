c13b322ae78657b37cba28c278c9be1d9adb9ce1
Syed Atyab Hussain <syed.atyab@codeline.rihal.om>
2022-12-11 02:00:50 -0800

    Added new project files

---
+++ src/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author SyedAtyabHussain
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nothing to see here");
+
+	}
+
+}
